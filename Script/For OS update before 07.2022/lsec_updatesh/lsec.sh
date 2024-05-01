ad7440bb29bacb6924507cd2aa0592969efbe8a9
Nikita Sidorov <4428551+shlyahten@users.noreply.github.com>
2024-05-01 19:01:04 +0300

    Initial commit

---
+++ Script/For OS update before 07.2022/lsec_updatesh/lsec.sh
@@ -0,0 +1,9 @@
+#!/system/bin/sh
+#
+
+mkdir /storage/sdcard1/DUMP
+dd if=/dev/block/mmcblk0boot0 of=/storage/sdcard1/DUMP/ROM2_boot0.bin
+dd if=/dev/block/mmcblk0boot1 of=/storage/sdcard1/DUMP/ROM3_boot1.bin
+dd if=/dev/block/mmcblk0 of=/storage/sdcard1/DUMP/GPT_Table.bin bs=1024 count=1024 
+dd if=/dev/block/mmcblk0 bs=1024 count=2108928 | gzip > /storage/sdcard1/DUMP/ROM1.bin.gz.001
+dd if=/dev/block/mmcblk0 bs=1024 count=2108928 skip=2108928 | gzip > /storage/sdcard1/DUMP/ROM1.bin.gz.002
